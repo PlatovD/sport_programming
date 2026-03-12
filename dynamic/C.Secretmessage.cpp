c12dec779973b0c43919ad678a8fd02ceda89de7
PlatovD <platovdima6@gmail.com>
2026-03-12 18:27:38 +0300

    Div 2 C

--- dynamic/C.Secretmessage.cpp
+++ dynamic/C.Secretmessage.cpp
@@ -38,17 +38,61 @@ int main() {
     while (t--) {
         int n, k;
         cin >> n >> k;
-        vector<string> lines(n);
+        vector<string> lines(k);
         for (auto &el: lines) cin >> el;
 
         // первым этапом пройдусь и на каждую позицию сделаю массив из 26 букв по их присутствию
         vector existing_chars_on_positions(n, vector<bool>(26));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < k; j++) {
-                existing_chars_on_positions[i][lines[i][j] - 'a'] = true;
+                existing_chars_on_positions[i][lines[j][i] - 'a'] = true;
             }
         }
 
+        // буду подбирать минимальный возможный step
+        int res_step = n;
+        vector<char> res_vector;
+        for (int step = 1; step <= n; step++) {
+            if (n % step != 0)
+                continue;
+            int cnt_chars_required_for_step = step;
+            vector<char> chars(cnt_chars_required_for_step);
+            // перебор позиций начала текущей буквы
+            bool is_bad_step_generally = false;
+            for (int s = 0; s < cnt_chars_required_for_step; s++) {
+                // ищу на все позции с началом в s и шагом step нужную букву
+                bool is_found = false;
+                for (int ch = 0; ch < 26; ch++) {
+                    bool is_bad = false;
+                    for (int pos = s; pos < n; pos += step) {
+                        if (!existing_chars_on_positions[pos][ch]) {
+                            is_bad = true;
+                            break;
+                        }
+                    }
+                    if (is_bad) {
+                        continue;
+                    }
+                    chars[s] = static_cast<char>('a' + ch);
+                    is_found = true;
+                    break;
+                }
+                if (!is_found) {
+                    is_bad_step_generally = true;
+                    break;
+                }
+            }
+            if (is_bad_step_generally) continue;
+            res_step = step;
+            res_vector = chars;
+            break;
+        }
 
+        for (int i = 0; i < n / res_step; i += 1) {
+            for (int j = 0; j < res_step; j++) {
+                cout << res_vector[j];
+            }
+        }
+        cout << '\n';
     }
 }
