74769a83055733b74db2862bd6add67d4c76bde2
PlatovD <platovdima6@gmail.com>
2026-07-31 10:10:32 +0300

    Дорешал Группа C. Битовые операции

--- bit/B4. Игрушки.cpp
+++ bit/B4. Игрушки.cpp
@@ -19,64 +19,93 @@
 #include <assert.h>
 
 #define ll long long
-// #define _DEBUG
+#define ld long double
 using namespace std;
 
-ll MOD = 1e9 + 7;
+void print(vector<string> &rgs) {
+    vector<string> ans;
+    for (string s: rgs) {
+        map<int, vector<int> > heaps;
+        for (int i = 0; i < s.size(); i++) {
+            heaps[s[i] - '0'].push_back(i + 1);
+        }
 
-// шаблон
-int main() {
-#if defined _DEBUG
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    std::ios::sync_with_stdio(false);
-    cout << fixed << setprecision(10);
-    cin.tie(0);
-    cout.tie(0);
-    int n;
-    cin >> n;
-    vector v(n * n, 0);
-    for (int i = n * (n - 1); i < n * n; i++) {
-        v[i] = 1;
+        for (auto &kv: heaps) {
+            sort(kv.second.begin(), kv.second.end());
+        }
+
+        string res;
+        res.reserve(10);
+        bool first = true;
+        for (auto &kv: heaps) {
+            if (!first) {
+                res += ',';
+            } else {
+                first = false;
+            }
+            res += '{';
+
+            for (int i = 0; i < kv.second.size(); i++) {
+                if (i != 0) res += ',';
+                res += to_string(kv.second[i]);
+            }
+            res += '}';
+        }
+        ans.push_back(res);
     }
 
-    set<string> already_added;
-    vector<string> ans;
-c:
-    do {
-        string total;
-        total.reserve(10);
-        bool is_first = true;
-        for (int i = 0; i < n; i++) {
-            string cur;
-            cur.reserve(10);
-            bool first = true;
-            for (int j = i * n; j < (i + 1) * n; j++) {
-                if (v[j]) {
-                    if (first) {
-                        cur += to_string(j % n + 1);
-                        first = false;
-                    } else {
-                        cur += "," + to_string(j % n + 1);
-                    }
-                }
-            }
-            if (!cur.empty()) {
-                cur = "{" + cur + "}";
-                if (!is_first)
-                    total += "," + cur;
-                else {
-                    is_first = false;
-                    total += cur;
-                }
-            }
-        }
-        if (already_added.find(total) != already_added.end()) continue;
-        already_added.insert(total);
-        ans.push_back(total);
-    } while (next_permutation(v.begin(), v.end()));
+    cout << ans.size() << '\n';
+    for (auto &s: ans) {
+        cout << s << '\n';
+    }
+}
 
-    cout << ans.size() << endl;
-    for (const auto &num: ans) cout << num << "\n";
+vector<string> next_rgs(vector<string> &current_rgs) {
+    vector<string> next_rgs;
+
+    for (int i = 1; i <= current_rgs.size(); i++) {
+        const string &old_rgs = current_rgs[i - 1];
+        int m = 0;
+        for (int j = 0; j < old_rgs.size(); j++) {
+            m = std::max(old_rgs[j] - '0', m);
+        }
+        m++;
+
+        if (i % 2 == 1) {
+            next_rgs.push_back(old_rgs + "0");
+            for (int j = m; j > 0; j--) {
+                next_rgs.push_back(old_rgs + to_string(j));
+            }
+        } else {
+            for (int j = 1; j <= m; j++) {
+                next_rgs.push_back(old_rgs + to_string(j));
+            }
+            next_rgs.push_back(old_rgs + "0");
+        }
+    }
+
+    return next_rgs;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<string> res = {"0"};
+    for (int i = 0; i < n - 1; i++) {
+        res = next_rgs(res);
+    }
+    print(res);
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
